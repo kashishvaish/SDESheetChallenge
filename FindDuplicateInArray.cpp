e64e5b961aa3724c309ff4709961c771d37e4d7e
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2022-06-11 23:16:10 +0530

    Cpp Solution of Find Duplicate In Array

--- FindDuplicateInArray.cpp
+++ FindDuplicateInArray.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h> 
 int findDuplicate(vector<int> &arr, int n){
+// 	Time: O(n)  Space: O(1)
 	for(int i = 0; i < n; i++) {
         if(arr[abs(arr[i])] < 0) {
             return abs(arr[i]);
